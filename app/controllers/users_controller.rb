23591fde443e1fed2d14812d0a3718278e96d4e7
Bea Gujuli <beatriz.gujuli@gmail.com>
2014-11-24 11:21:31 +0100

    Begining with Devise

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,14 @@
+class UsersController < ApplicationController
+	before_action :authenticate_user!, only: :show
+
+	def show
+		@user = User.find(params[:id])
+		render 'users/show'
+	end
+
+	def create
+		super do |resource|
+			puts "#{resource.name}"
+		end
+	end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,13 @@
 Rails.application.routes.draw do
 
+  devise_for :users, path: '',
+                     path_names: { sign_in: 'login', sign_up: 'signup'},
+                     controllers: { sessions: 'users/sessions'}
+
+  get 'users/:id', to: 'users#show'
+
   resources :stories
-    member do
-      get 'new'
-    end
-
-
+  
   get 'home' => 'home#index'
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
